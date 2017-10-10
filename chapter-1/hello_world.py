fcc3f3cfba6093f6207561d94f82b1d7beca99bd
yuezht <751349974@qq.com>
2017-10-10 14:39:08 +0800

    hello world

--- chapter-1/hello_world.py
+++ chapter-1/hello_world.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+# -*- coding: UTF-8 -*-
 
 import sys
 
@@ -20,3 +21,13 @@ print str[2:5]  # 输出字符串中第三个至第五个之间的字符串
 print str[2:]  # 输出从第三个字符开始的字符串
 print str * 2  # 输出字符串两次
 print str + "TEST"  # 输出连接的字符串
+
+list = ['runoob', 786, 2.23, 'john', 70.2]
+tinylist = [123, 'john']
+
+print list  # 输出完整列表
+print list[0]  # 输出列表的第一个元素
+print list[1:3]  # 输出第二个至第三个的元素
+print list[2:]  # 输出从第三个开始至列表末尾的所有元素
+print tinylist * 2  # 输出列表两次
+print list + tinylist  # 打印组合的列表
