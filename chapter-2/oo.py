5ff4b5c054aad99acf589eef35c86c4c4b7a42f6
yuezht <751349974@qq.com>
2017-10-10 18:09:20 +0800

    flask server

--- chapter-2/oo.py
+++ chapter-2/oo.py
@@ -1,7 +1,21 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 
+class Employee:
+   '所有员工的基类'
+   empCount = 0
 
-def print_func( par ):
-   print "Hello : ", par
-   return
+   def __init__(self, name, salary):
+      self.name = name
+      self.salary = salary
+      Employee.empCount += 1
+
+   def displayCount(self):
+      print "Total Employee %d" % Employee.empCount
+
+   def displayEmployee(self):
+      print "Name : ", self.name, ", Salary: ", self.salary
+
+   def print_func( par ):
+      print "Hello : ", par
+      return
---
+++ chapter-2/oo_test.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+import oo
+
+
+emp1 = oo.Employee("Tom",13)
+emp2 = oo.Employee("Jim", 15)
+
+print emp2.displayCount()
+
+print emp1.__class__
+print emp1.__doc__
+
+
+print emp2.__class__
+
