b799481a8dba287ddd8c7107525465b2572906a0
yuezht <751349974@qq.com>
2017-10-10 15:26:17 +0800

    hello world

--- chapter-1/hello_world.py
+++ chapter-1/hello_world.py
@@ -2,7 +2,8 @@
 # -*- coding: UTF-8 -*-
 
 import sys
-
+import os
+import time
 
 print "------------------------"
 print sys.copyright
@@ -31,3 +32,30 @@ print list[1:3]  # 输出第二个至第三个的元素
 print list[2:]  # 输出从第三个开始至列表末尾的所有元素
 print tinylist * 2  # 输出列表两次
 print list + tinylist  # 打印组合的列表
+
+dict = {}
+dict['one'] = "This is one"
+dict[2] = "This is two"
+
+tinydict = {'name': 'john', 'code': 6734, 'dept': 'sales'}
+
+print dict['one']  # 输出键为'one' 的值
+print dict[2]  # 输出键为 2 的值
+print tinydict  # 输出完整的字典
+print tinydict.keys()  # 输出所有键
+print tinydict.values()  # 输出所有值
+
+
+print time.time()
+
+def  f(param):
+    return 'hello'+param
+
+print f("world!")
+
+
+import module
+module.print_func("test")
+
+from math import sin
+print sin(9)
