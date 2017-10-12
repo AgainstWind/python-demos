22a6e70df472aee2ac0abf0ad4229779664d64f4
yuezht <75134974@qq.com>
2017-10-12 20:59:09 +0800

    api request

--- flask-server/test.py
+++ flask-server/test.py
@@ -1,7 +1,7 @@
-import requests
+import requests,json
 
-urll = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
-r=requests.get(urll)
+url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
+r=requests.get(url)
 
 #print r.headers()
 print '**************************'
