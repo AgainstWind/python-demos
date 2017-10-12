9b0fcb19bb4378d8198ee40a7609a1a67118e68c
yuezht <75134974@qq.com>
2017-10-12 21:57:37 +0800

    update

--- data-analytics/numpy_demos.py
+++ data-analytics/numpy_demos.py
@@ -2,4 +2,15 @@ import numpy
 import pandas
 
 
+arrays = [[1,2,3],[3,5,2],[6,3,1]]
+narrays = numpy.array(arrays)
+
+print narrays
+
+print numpy.eye(5,3,k=0,dtype=float)
+
+a=1+2j
+b=4+1j
+print a+b
+
 
