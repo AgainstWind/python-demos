22bce0f1ff48393d52a7cfa518e1e668e5d4fc17
yuezht <75134974@qq.com>
2017-10-17 20:47:57 +0800

    linear algebra 1

--- mathematics/linear_algebra_test.py
+++ mathematics/linear_algebra_test.py
@@ -6,11 +6,12 @@ import numpy as np
 
 def matrix_test():
     a = np.matrix([[1,4,3],[6,3,8],[-1,5,3]])
-    print a.shape
     print a
+    print a.shape
     print a.T
     print a.A
     print a.A1
     print np.linalg.det(a) #hang lie shi
+    print np.linalg.det(a.T)
 
 matrix_test()
