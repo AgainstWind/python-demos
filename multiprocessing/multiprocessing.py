54d825ce7bc611de02c9ec12cc8b05750729214f
yuezht <75134974@qq.com>
2017-10-18 14:19:18 +0800

    multi process

--- multiprocessing/multiprocessing.py
+++ multiprocessing/multiprocessing.py
@@ -2,11 +2,19 @@
 # -*- coding: UTF-8 -*-
 
 import os
+#from multiprocessing import Process # windows
 
+def unixTest():
+    print 'Process (%s) start...' %os.getpid()
+    pid = os.fork()
+    if pid==0:
+        print 'I\'m child process: %s, and my parent is %s.' %(os.getpid(),os.getppid())
+    else:
+        print 'I\'m parent process: %s, child process: %s.' %(os.getpid(),pid)
 
-print 'Process (%s) start...' %os.getpid()
-pid = os.fork()
-if pid==0:
-    print 'I\'m child process: %s, and my parent is %s.' %(os.getpid(),os.getppid())
-else:
-    print 'I\'m parent process: %s, child process: %s.' %(os.getpid(),pid)
+def windowsTest():
+    print '...'
+
+if __name__=='__main__':
+    unixTest()
+    windowsTest()
